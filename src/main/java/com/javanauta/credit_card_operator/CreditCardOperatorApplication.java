0a8e1e500562d9bfdceb20f9a2fa6aa4afbf45a2
Marina <marinabrito.ma@gmail.com>
2025-01-19 17:30:53 +0000

    first commit

---
+++ src/main/java/com/javanauta/credit_card_operator/CreditCardOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.credit_card_operator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardOperatorApplication.class, args);
+	}
+
+}
