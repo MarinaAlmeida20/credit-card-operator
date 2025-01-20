31defed4d9e8f07fe608d185287d007ba85851a9
Marina <marinabrito.ma@gmail.com>
2025-01-20 21:57:53 +0000

    client mapper

--- src/main/java/com/javanauta/credit_card_operator/adaptor/mapper/ClientMapper.java
+++ src/main/java/com/javanauta/credit_card_operator/adaptor/mapper/ClientMapper.java
@@ -1,8 +1,12 @@
 package com.javanauta.credit_card_operator.adaptor.mapper;
 
+import com.javanauta.credit_card_operator.adaptor.in.dto.request.ClientRequestDto;
+import com.javanauta.credit_card_operator.adaptor.in.dto.response.ClientResponseDto;
+import com.javanauta.credit_card_operator.adaptor.out.entity.CardEntity;
 import com.javanauta.credit_card_operator.adaptor.out.entity.ClientEntity;
 import com.javanauta.credit_card_operator.application.domain.ClientDomain;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
@@ -11,4 +15,12 @@ public interface ClientMapper {
     ClientEntity toEntity(ClientDomain clientDomain);
 
     ClientDomain toDomain(ClientEntity clientEntity);
+
+    @Mapping(target = "card", expression = "java(toCardEntity(client))")
+    ClientEntity toEntity(ClientRequestDto client);
+
+    ClientResponseDto toResponse(ClientEntity client);
+
+    @Mapping(source = "invoice due date", target = "invoiceDueDate")
+    CardEntity toCardEntity(ClientRequestDto clientRequestDto);
 }
